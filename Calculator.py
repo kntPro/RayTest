ca97cf8a1078fbc072293b2e90f4e0593cf1bd03
kento <kentoyamamoto26@gmail.com>
2024-05-04 17:58:09 +0900

    commit

--- Calculator.py
+++ Calculator.py
@@ -3,23 +3,26 @@ import numpy as np
 from Memory import Memory
 from tqdm import tqdm
 import time
+import torch
 
-@ray.remote
+@ray.remote(num_gpus=0.5)
 class Calculator:
-    def __init__(self, memlen:int, end:int) -> None:
-        self.log = list()
-        self.mem = Memory(memlen)
+    def __init__(self, end:int) -> None:
         self.max_calc_num = end
         self.calc_count=0
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
     def calc(self) -> bool:
         with tqdm(total=self.max_calc_num) as pbar:
             while self.calc_count < self.max_calc_num:
-                if self.mem.size() >= 1:
+                try:
                     mat, vec = self.mem.pop()
-                    self.log.append(np.matmul(mat, vec))
-                    pbar.update(1)
-                    self.calc_count+=1
+                except:
+                    continue
+                #torch.matmul(torch.from_numpy(mat).to(device=self.device),torch.from_numpy(vec).to(device=self.device))
+                np.matmul(mat,vec)
+                pbar.update(1)
+                self.calc_count+=1
         return True
 
     def add(self, mat:np.ndarray, vec:np.ndarray) -> None:
--- main.py
+++ main.py
@@ -6,25 +6,30 @@ import time
 from copy import deepcopy
 
 CALC_NUM = int(1e4)
-SHAPE = 100
+SHAPE = 1024
 ray.init()
 
 def main():
-    calculator = Calculator.remote(memlen=CALC_NUM,end=CALC_NUM)
+    memory = Memory(100)
+    calculators = [Calculator.remote(end=CALC_NUM) for _ in range(3)]
     start = time.time()
+    
+    #ここで、ray.get([process...])にしてしまうと、calculator.calcとprocessが同時に終了するまで待ってしまい 
+    #calculatorのmemが貯まらないため一生終わらない
+    mems = mem_loop.remote(memory) 
+    calc = [calculator.calc.remote() for calculator in calculators]
+    done = ray.wait([mems,*calc])
+    print(f"tasks complete:{time.time()-start}s")
+
+@ray.remote
+def mem_loop(memory:Memory):
     count = 0
     while count<CALC_NUM:
         mat = np.random.randn(SHAPE,SHAPE)
         vec = np.random.randn(SHAPE)
-        calculator.add.remote(mat, vec)
+        memory.add(mat, vec)
         count+=1
     
-    #ここで、ray.get([process...])にしてしまうと、calculator.calcとprocessが同時に終了するまで待ってしまうので
-    #calculatorのmemが貯まらないため一生終わらない
-    done = ray.wait(calculator.calc.remote())
-    print(ray.get(done))
-    print(f"tasks complete:{time.time()-start}s")
-
 
 
 if __name__=="__main__":
