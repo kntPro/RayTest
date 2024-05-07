7ec4d02620fd4cd61585b968a83fe3ae1a67ccb5
kento <kentoyamamoto26@gmail.com>
2024-05-07 16:51:21 +0900

    set default

--- Calculator.py
+++ Calculator.py
@@ -5,22 +5,28 @@ from tqdm import tqdm
 import time
 import torch
 
+CALCULATOR_NUM=3
+
 @ray.remote
 class Calculator:
-    def __init__(self, memory:Memory, start:int) -> None:
+    def __init__(self, memory:Memory, start:int, end:int) -> None:
         self.start_calc_num = start
+        self.end_calc_num = end
         self.mem = memory
         self.calc_count=0
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
     def calc(self) -> bool:
-        while self.calc_count <= self.start_calc_num:
+        while self.calc_count < self.end_calc_num:
+            if self.calc_count <= self.start_calc_num:
+                continue
             mat, vec = self.mem.sample()
-            while bool(mat or vec):
-                #torch.matmul(torch.from_numpy(mat).to(device=self.device),torch.from_numpy(vec).to(device=self.device))
-                np.matmul(mat,vec)
-                mat, vec = self.mem.sample()
-
+            try:
+                assert mat or vec, f"mat was {mat},   vec was {vec}"
+            except AssertionError as err:
+                print(f"Calculator have not been able to sample from memory \n {err}")
+            #torch.matmul(torch.from_numpy(mat).to(device=self.device),torch.from_numpy(vec).to(device=self.device))
+            np.matmul(mat,vec)
             self.calc_count+=1
         return True
 
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import ray
 import numpy as np
 from Memory import Memory
-from Calculator import Calculator
+from Calculator import Calculator, CALCULATOR_NUM
 import time
 from copy import deepcopy
 
-CALC_NUM = int(1e3)
+CALC_NUM = int(2**8)
 SHAPE = 1024
-CALUCLATOR_NUM = 3
+MEMORY_SIZE = 1024
 ray.init()
 
 def main():
-    memory = Memory(CALC_NUM)
-    calculators = [Calculator.remote(memory=memory, start=CALC_NUM/CALUCLATOR_NUM) for _ in range(CALUCLATOR_NUM)]
+    memory = Memory(MEMORY_SIZE)
+    calculators = [Calculator.remote(memory=memory, start=128, end=CALC_NUM) for _ in range(CALCULATOR_NUM)]
     start = time.time()
     
     #ここで、ray.get([process...])にしてしまうと、calculator.calcとprocessが同時に終了するまで待ってしまい 
@@ -20,17 +20,16 @@ def main():
     mems = mem_loop.remote(memory) 
     calc = [calculator.calc.remote() for calculator in calculators]
     done = ray.wait([mems,*calc])
+    done = [ray.get(obj) for obj in done]
     print(done)
     print(f"tasks complete:{time.time()-start}s")
 
 @ray.remote
 def mem_loop(memory:Memory):
-    count = 0
-    while count<CALC_NUM:
+    for _ in range(MEMORY_SIZE*2):
         mat = np.random.randn(SHAPE,SHAPE)
         vec = np.random.randn(SHAPE)
         memory.add(mat, vec)
-        count+=1
     return True
     
 
